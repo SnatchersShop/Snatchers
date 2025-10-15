fd778aba87f00015cf11d720c71003868c411ed9
SnatchersShop <snatchers.shop@gmail.com>
2025-10-16 02:02:54 +0530

    server.js - backend-4

--- Snatchers_Backend/snatchers-backend-main/server.js
+++ Snatchers_Backend/snatchers-backend-main/server.js
@@ -47,7 +47,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://www.snatchers.in"
+  "https://www.snatchers.in",
   "https://zupra.online"
 ];
 
